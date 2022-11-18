210558274c38db1a915f84ac13549fb746d63ea6
Sujan Singh Dangi <sujandangi@gmail.com>
2022-11-18 22:26:44 +0530

    final build after completing tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  //Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+
+  //Fetch tasks singular
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+
+    // commented out after implementing json server
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+  };
+
+  // Delete Task (and send as props to Task component)
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
+
+    setTasks(tasks.filter((task) => task.id !== id));
+    console.log("Deleted");
+  };
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
+    console.log("Reminder Toggled", id);
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+
+        {showAddTask && <AddTask onAdd={addTask} />}
+
+        {tasks.length > 0 ? (
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          />
+        ) : (
+          "No Tasks To Show"
+        )}
+
+        <Footer />
+      </div>
   );
 }
 
---
+++ src/components/Tasks.js
@@ -0,0 +1,32 @@
+// const tasks = [
+//     {
+//         id: 1,
+//         text: 'Doctors Appointment',
+//         day: 'Feb 5th at 2:30pm',
+//         reminder: true,
+//     },
+//     {
+//         id: 2,
+//         text: 'Meeting at School',
+//         day: 'Feb 6th at 1:30pm',
+//         reminder: true,
+//     },
+//     {
+//         id: 3,
+//         text: 'Food Shopping',
+//         day: 'Feb 5th at 2:30pm',
+//         reminder: false,
+//     }
+// ]
+
+import Task from "./Task"
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+    return (
+    <>
+        {tasks.map((task) => ( <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} /> ))}
+    </>
+  )
+}
+
+export default Tasks
